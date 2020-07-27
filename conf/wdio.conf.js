ecdb8ed4dc0e5f222bea0fa31351e6278e33e89c
Amit Saldi <amit.saldi@gmail.com>
2020-07-27 11:22:32 +1000

    chrome headless

--- conf/wdio.conf.js
+++ conf/wdio.conf.js
@@ -5,6 +5,11 @@ exports.config = {
     capabilities: [{
         maxInstances: 5,
         browserName: 'chrome',
+        'goog:chromeOptions': {
+            // to run chrome headless the following flags are required
+            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+            args: ['--headless', '--disable-gpu'],
+            }
     }],
     logLevel: 'info',
     baseUrl: 'http://automationpractice.com',
